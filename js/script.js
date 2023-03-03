273306d752474b2e85b67634e04b82618daa87d6
xonic5461@gmail.com <85699535+wag154@users.noreply.github.com>
2023-03-03 11:10:44 +0000

    Added word checker

--- js/script.js
+++ js/script.js
@@ -1,25 +1,61 @@
 const time = document.querySelector("#Timer");
 const DisplayWords = document.querySelector("#Words")
 const form = document.querySelector("#Dis")
+var display = document.querySelector("#TextDisplay")
+const Words = ["hello","the","end","world","word","blue"];
+
+var TimerUp = false;
 
 let TimeSecond = 60;
+
+const CheckWords = (words) =>{
+
+  let wordsArr = [];
+  let word = "";
+  let correctWordCount = 0;
+
+  for (let i = 0; i < words.length;i++){
+    if (words[i] != " "){
+      word += words[i].toLowerCase();
+    }
+
+    else {
+      wordsArr.push(word);
+      word = "";
+    }
+  }
+  for (let i =0 ; i < words.length;i++){
+    Words.forEach((word) => {
+      if (wordsArr[i] == word){
+        correctWordCount ++;
+      }
+    })
+  }
+  display.textContent = `You got ${correctWordCount} correct!`;
+}
 
 const countDown = setInterval (()=>{
   TimeSecond --;
   time.textContent = `${TimeSecond} Seconds`
-  if (TimeSecond <= 0||TimeSecond < 1){
-    clearInterval(countDown);
-  }
+ if (TimeSecond == 0){
+   TimerUp = true;
+   time.textContent = "TIME UP!"
+   clearInterval(countDown);
+ }
 },1000)
-const TimerOver = ()=>{
-  
+
+const TimerOver = (e)=>{
+  e.preventDefault()
+  CheckWords(e.target.TextInput.value)
+  e.target.TextInput.value = "";
+
 }
 async function GetWords (Difficulty) {
   try {
     const resp = await fetch(`127.0.0.1:3000/Get${Difficulty}Words`)
     if (resp.ok){
-      const data = resp.json()
-
+      const data = await resp.json()
+      CheckWords(data);
     }
     else {
       throw "Error status code :",resp.status;
@@ -28,12 +64,11 @@ async function GetWords (Difficulty) {
   catch{((e)=> console.log(e))}
 }
 
-const ExampleWords = () =>{
-  const Words = ["hello","the","end","world","word","blue"];
-}
-
 async function timeTaken(Complete){
   
 }
 
-form.addEventListener("Submit",TimerOver)
+form.addEventListener("submit",TimerOver)
+form.addEventListener("easy",easyWords)
+form.addEventListener("medium",mediumWords)
+form.addEventListener("hard",hardWords)
