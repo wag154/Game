3bb058fd11529a16a7ca6543ca2879b30920d34e
MHaroonNasir <hrn-nsr@hotmail.com>
2023-03-03 14:11:17 +0000

    increased readability

--- server/app.js
+++ server/app.js
@@ -9,12 +9,17 @@ app.use(cors());
 const jsonData = require("./data.json");
 const internal = require("stream");
 
-const getWords = (difficultyNumber, res) => {
-    let wordArr = [];
-    wordArr.push(jsonData[0].text)
-    console.log(wordArr)
-    let newWordArr = "";
+const addLettersToEmptyString = (indexOfSpaceBeforeWord, indexOfSpaceAfterWord, newWordArr, wordArr) => {
+    let pushCounter = indexOfSpaceBeforeWord+1;
+    while (pushCounter != indexOfSpaceAfterWord-1) {
+        newWordArr += wordArr[0][pushCounter];
+        pushCounter += 1;
+    }
+    newWordArr += " ";
+    return newWordArr;
+}
 
+const getRandomWords = (difficultyNumber, wordArr, newWordArr) => {
     let randomNumber = Math.floor(Math.random()*difficultyNumber)+1;
     let indexOfSpaceBeforeWord;
     let indexOfSpaceAfterWord;
@@ -39,14 +44,20 @@ const getWords = (difficultyNumber, res) => {
         }
         randomNumber = Math.floor(Math.random()*difficultyNumber)+1;
 
-        let pushCounter = indexOfSpaceBeforeWord+1;
-        while (pushCounter != indexOfSpaceAfterWord-1) {
-            newWordArr += wordArr[0][pushCounter];
-            pushCounter += 1;
-        }
-        newWordArr += " ";
-
+        newWordArr = addLettersToEmptyString(indexOfSpaceBeforeWord, indexOfSpaceAfterWord, newWordArr, wordArr);
     }
+    return newWordArr;
+}
+
+const getWords = (difficultyNumber, res) => {
+    let wordArr = [];
+    wordArr.push(jsonData[0].text)
+    let newWord = "";
+    let newWordArr = [];
+
+    newWord = getRandomWords(difficultyNumber, wordArr, newWord);
+    newWordArr.push(newWord);
+
     res.send(newWordArr)
 }
 
