3a7063e335d6d3efd1c8804e6a4cf0118466e73b
xonic5461@gmail.com <85699535+wag154@users.noreply.github.com>
2023-03-03 10:12:25 +0000

    Added fetch function

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
 const time = document.querySelector("#Timer");
-let TimeSecond = 5;
-time.textContent = `00:${TimeSecond}`;
+let TimeSecond = 60;
 
 const countDown = setInterval (()=>{
   TimeSecond --;
-  time.textContent = `${TimeSecond}`
+  time.textContent = `${TimeSecond} Seconds`
   if (TimeSecond <= 0||TimeSecond < 1){
     clearInterval(countDown);
   }
 },1000)
+
+async function GetWords (Difficulty) {
+  try {
+    const resp = await fetch(`127.0.0.1:3000/Get${Difficulty}Words`)
+    if (resp.ok){
+      const data = resp.json()
+
+    }
+    else {
+      throw "Error status code :",resp.status;
+    }
+  }
+  catch{((e)=> console.log(e))}
+}
 
